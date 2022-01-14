29b5e77554d58dba1e619f3d8971d04a5b5c46ea
KlausJuhl <97036675+KlausJuhl@users.noreply.github.com>
2022-01-14 10:49:11 +0100

    Add files via upload

---
+++ SQL Scripts/Create Products.sql
@@ -0,0 +1,41 @@
+USE [SelfServicePOS]
+GO
+
+INSERT INTO [dbo].[Product]
+           ([ProductNumber]
+           ,[ProductName]
+           ,[Price]
+           ,[ImageFileName])
+     VALUES
+           (1
+           ,'Sodavand'
+           ,10
+           ,'Sodavand.jpg')
+
+
+INSERT INTO [dbo].[Product]
+           ([ProductNumber]
+           ,[ProductName]
+           ,[Price]
+           ,[ImageFileName])
+     VALUES
+           (2
+           ,'Øl'
+           ,10
+           ,'Øl.jpg')
+
+
+INSERT INTO [dbo].[Product]
+           ([ProductNumber]
+           ,[ProductName]
+           ,[Price]
+           ,[ImageFileName])
+     VALUES
+           (3
+           ,'Is'
+           ,10
+           ,'Is.jpg')
+
+GO
+
+
